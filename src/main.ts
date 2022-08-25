20772af6940385bf5ab550653a16fd5c35c130a7
Chris Moran <chris@chrismoran.com>
2022-08-25 10:39:06 -0400

    tweaked rsinstance: hasShadow: false, tweaked window/view tracking

--- src/main.ts
+++ src/main.ts
@@ -76,6 +76,14 @@ app.once("ready", () => {
 	if(app.dock) {
 		app.dock.hide();
 	}
+	if (process.platform === "darwin") {
+		if (!app.accessibilitySupportEnabled) {
+			app.setAccessibilitySupportEnabled(true);
+		}
+		if (app.dock) {
+			app.dock.hide();
+		}
+	}
 
 	globalShortcut.register("Alt+1", alt1Pressed);
 	drawTray();
