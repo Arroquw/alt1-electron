7f2a30fb10613ccc69ed40810ba730db5b007814
Justin van Son <justinvanson@outlook.com>
2026-01-07 00:04:25 +0100

    Move devDependencies to dependencies
    None were actually dev-only

--- flake.nix
+++ flake.nix
@@ -68,7 +68,7 @@
             npmDeps = pkgs.fetchNpmDeps {
               src = "${finalAttrs.src}";
               packageLock = "${finalAttrs.src}/package-lock.json";
-              hash = "sha256-vH+f8ALYfR5yfF/zYK3dPj0GdwcQP/XHC0WC4994D8c=";
+              hash = "sha256-7MAu8j9ZRbfKeYHObVBTNDwzVIOcGyqk6icGnSQP3QA=";
             };
             makeCacheWritable = true;
             env = {
